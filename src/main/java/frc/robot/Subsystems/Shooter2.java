8a5cb1c3f28e75b4b52f02ee6b9ed2cd73bc646f
Bronx High School of Science <School@Robotics-01.bxsci.78x445.nycboe.org>
2022-02-03 17:48:30 -0500

    added shooter2

---
+++ src/main/java/frc/robot/Subsystems/Shooter2.java
@@ -0,0 +1,19 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.Subsystems;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.DutyCycleEncoder;
+
+public class Shooter2 extends Subsystem {
+
+  //instantiate absolute encoder
+  @Override
+  public void initDefaultCommand() {
+    // Set the default command for a subsystem here.
+    // setDefaultCommand(new MySpecialCommand());
+  }
+
+}
