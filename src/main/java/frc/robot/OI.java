6bebc09717181de049338613ac299181d4a4041d
Bronx High School of Science <School@Robotics-01.bxsci.78x445.nycboe.org>
2022-02-05 16:12:55 -0500

    isolated drivetrain subsystem and commands

--- src/main/java/frc/robot/OI.java
+++ src/main/java/frc/robot/OI.java
@@ -3,31 +3,8 @@
 // the WPILib BSD license file in the root directory of this project.
 
 package frc.robot;
-import edu.wpi.first.wpilibj.buttons.JoystickButton;
-import frc.robot.Commands.ExtendPiston;
-import frc.robot.Commands.GetDistance;
-import frc.robot.Commands.RetractPiston;
-import frc.robot.Commands.ShootBall;
-import frc.robot.Commands.ShootBall2;
-import frc.robot.Commands.SpinMotors;
 import edu.wpi.first.wpilibj.Joystick;
 
 public class OI {
     public static Joystick driveJoy = new Joystick(RobotMap.driveJoyPort);
-    public static JoystickButton ultButton = new JoystickButton(driveJoy, 1);
-	public static JoystickButton scoopBall = new JoystickButton(driveJoy, 5);
-	public static JoystickButton noScoopBall = new JoystickButton (driveJoy, 6);
-    public static JoystickButton shootBall = new JoystickButton (driveJoy, 7);
-    public static JoystickButton spinMotors = new JoystickButton (driveJoy, 8);
-    public static JoystickButton shooter2 = new JoystickButton (driveJoy, 9);
-
-
-    public void bindButtons(){
-        shootBall.whenPressed(new ShootBall()); //you must have new
-        ultButton.whenPressed(new GetDistance());
-        scoopBall.whenPressed(new ExtendPiston());
-        noScoopBall.whenPressed(new RetractPiston());
-        spinMotors.whileHeld(new SpinMotors());
-        shooter2.whenPressed(new ShootBall2());
-    }
 }
