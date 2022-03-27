3a0648db8370e6a501d89621a0fe94da8b632418
Bronx High School of Science <School@Robotics-03.bxsci.78x445.nycboe.org>
2022-03-27 10:04:58 -0400

    added driveAutonGroup into CommandGroups folder

--- src/main/java/frc/robot/CommandGroups/DriveAutonGroup.java
+++ src/main/java/frc/robot/CommandGroups/DriveAutonGroup.java
@@ -2,9 +2,11 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-package frc.robot.Commands;
+package frc.robot.CommandGroups;
 
 import edu.wpi.first.wpilibj.command.CommandGroup;
+import frc.robot.Commands.DriveAuton;
+import frc.robot.Commands.DriveStraight;
 
 public class DriveAutonGroup extends CommandGroup {
   /** Add your docs here. */
